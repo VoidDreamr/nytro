56c32718d9104be19d47c8dce7b1971b6acc4db2
VoidDreamr <voiddreamr@gmail.com>
2026-05-09 20:17:49 -0500

    Added assembly parsing.

--- bin/nytro.dart
+++ bin/nytro.dart
@@ -1,29 +1,12 @@
-import 'package:nytro/core/instructions/ny_instr.dart';
-import 'package:nytro/core/instructions/ny_operand.dart';
+import 'package:nytro/core/ny.dart';
 import 'package:nytro/core/ny_program.dart';
+import 'dart:io';
 
-void main(List<String> arguments) {
-  final program = NyProgram();
-  program.instructions.add(
-    NyInstrCode.write.create([
-      NyConstant(value: 1),
-      NyRegisterRef(slot: 0, offset: 0),
-    ]),
-  );
-  program.instructions.add(
-    NyInstrCode.write.create([
-      NyConstant(value: 2),
-      NyRegisterRef(slot: 1, offset: 0),
-    ]),
-  );
-  program.instructions.add(
-    NyInstrCode.write.create([
-      NyConstant(value: 3),
-      NyRegisterRef(slot: 1, offset: 0),
-      NyRegisterRef(slot: 1, offset: 1),
-    ]),
-  );
+void main(List<String> arguments) async {
+  final src = await File('example.nyasm').readAsString();
 
+  NyProgram program = Ny.createAssemblyProgram(src);
   program.run();
+
   print(program.register.dump());
 }
--- lib/core/instructions/ny_instr.dart
+++ lib/core/instructions/ny_instr.dart
@@ -14,9 +14,16 @@ enum NyInstrCode {
   static final Map<NyInstrCode, NyInstr Function(List<NyOperand>)> _factories =
       {NyInstrCode.write: NyWriteInstr.fromOperands};
 
+  static final Map<String, NyInstrCode> _mnemonics = {
+    for (final code in NyInstrCode.values) code.mnemonic: code,
+  };
+
   final String mnemonic;
 
   const NyInstrCode({required this.mnemonic});
 
   NyInstr create(List<NyOperand> ops) => _factories[this]!(ops);
+
+  static NyInstrCode? fromMnemonic(String mnemonic) =>
+      _mnemonics[mnemonic.toUpperCase()];
 }
