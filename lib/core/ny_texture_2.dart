7036ae7edfc37a622af760c5a6e5f1487a90e8e9
VoidDreamr <voiddreamr@gmail.com>
2026-05-12 20:30:51 -0500

    Improved `SAMPLE_TEX2` performance.

--- lib/core/ny_texture_2.dart
+++ lib/core/ny_texture_2.dart
@@ -4,13 +4,20 @@ import 'package:nytro/core/ny_type.dart';
 import 'package:vector_math/vector_math.dart';
 
 class NyTexture2 {
+  final Vector4 _top = Vector4.zero();
+  final Vector4 _bot = Vector4.zero();
+  final Vector4 _tl = Vector4.zero();
+  final Vector4 _tr = Vector4.zero();
+  final Vector4 _bl = Vector4.zero();
+  final Vector4 _br = Vector4.zero();
+
   static const int headerSize = 16;
 
   final int channels = 4;
 
   Float32List buffer;
-  int get width => buffer[0].toInt();
-  int get height => buffer[1].toInt();
+  int get width => buffer[0].truncate();
+  int get height => buffer[1].truncate();
 
   NyTexture2({required this.buffer});
 
@@ -31,30 +38,23 @@ class NyTexture2 {
     return NyTypes.vec4.unpack(buffer, index(x, y), out);
   }
 
-  Vector4 sample(double u, double v, [Vector4? out]) {
-    out ??= Vector4.zero();
-
-    final x = u.clamp(0.0, 1.0) * (width - 1);
-    final y = v.clamp(0.0, 1.0) * (height - 1);
+  void sample(double u, double v, Vector4 out) {
+    final x = u % 1.0 * (width - 1);
+    final y = v % 1.0 * (height - 1);
     final x0 = x.floor();
     final x1 = x.ceil();
     final y0 = y.floor();
     final y1 = y.ceil();
+    final xn = x - x0;
 
-    Vector4 tl = get(x0, y0);
-    Vector4 tr = get(x1, y0);
-    Vector4 bl = get(x0, y1);
-    Vector4 br = get(x1, y1);
+    NyTypes.vec4.unpack(buffer, index(x0, y0), _tl);
+    NyTypes.vec4.unpack(buffer, index(x1, y0), _tr);
+    NyTypes.vec4.unpack(buffer, index(x0, y1), _bl);
+    NyTypes.vec4.unpack(buffer, index(x1, y1), _br);
 
-    Vector4 top = Vector4.zero();
-    Vector4 bot = Vector4.zero();
-    double xn = x - x0;
-
-    Vector4.mix(tl, tr, xn, top);
-    Vector4.mix(bl, br, xn, bot);
-    Vector4.mix(top, bot, y - y0, out);
-
-    return out;
+    Vector4.mix(_tl, _tr, xn, _top);
+    Vector4.mix(_bl, _br, xn, _bot);
+    Vector4.mix(_top, _bot, y - y0, out);
   }
 
   void set(int x, int y, Vector4 color) {
